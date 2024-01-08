5d2f9b040e0abf2a57b3abed9dcd0d9e2ad82580
ketyung <ketyung@techchee.com>
2024-01-08 10:34:33 +0800

    Initial Commit

---
+++ SwiftUIImageFilterScrollView/SwiftUIImageFilterScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImageFilterScrollViewApp.swift
+//  SwiftUIImageFilterScrollView
+//
+//  Created by Christopher Chee on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImageFilterScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
